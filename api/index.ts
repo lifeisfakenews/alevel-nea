e9c899f724cc157194cd7e6f35bef5ad926f79f4
lifeisfakenews <lifeisfakenews69@gmail.com>
2025-10-18 18:12:14 +0100

    CKPNT: create user and login user finished + tested

--- api/index.ts
+++ api/index.ts
@@ -129,11 +129,36 @@ web_server.post("/users/login", async(req, res) => {
             }
         });
     } catch(e:any) {
-        log(`Error on PUT \`/users/login\`\n\`\`\`${e.message}\`\`\`\n\n\`\`\`${e.stack}\`\`\``, "error");
+        log(`Error on POST \`/users/login\`\n\`\`\`${e.message}\`\`\`\n\n\`\`\`${e.stack}\`\`\``, "error");
         return res.status(500).send("Internal server error");
     }
 });
 
+// perform required checks for usernames when account is created
+async function validateUsername(username: string) {
+    if (!username) return {valid: false, error: "No username provided"};
+    if (username.length < 3 || username.length >= 24) return {valid: false, error: "Username must be between 3 and 24 characters"};
+    // the regex matches to lower case letters, numbers and dashes
+    // if the full string doesnt match then the username contains disallowed characters
+    if (!username.match(/^[a-z0-9-]+$/)) return {valid: false, error: "Username can only contain letters, numbers, and dashes"};
+
+    const user = await db_users.findOne({ username });
+    if (user) return {valid: false, error: "Username already taken"};
+
+    return {valid: true, error: null};
+};
+
+//perform required checks for passwords when account is created
+function validatePassword(password: string) {
+    if (!password) return {valid: false, error: "No password provided"};
+    if (password.length < 6) return {valid: false, error: "Password must be at least 6 characters"};
+    if (!password.match(/[a-z]/)) return {valid: false, error: "Password must contain at least one lowercase letter"};
+    if (!password.match(/[A-Z]/)) return {valid: false, error: "Password must contain at least one uppercase letter"};
+    if (!password.match(/[0-9]/)) return {valid: false, error: "Password must contain at least one number"};
+    //The \W group matches any non-word character (i.e. symbols). _ must also be specified as it is considered a word character
+    if (!password.match(/[\W_]/)) return {valid: false, error: "Password must contain at least one special character"};
+    return {valid: true, error: null};
+}
 
 // create a new user. Body requires `username`, `password`, `name`, and `role`
 // TODO: This will be restricted to staff only, however to create test accounts easily it isnt.
@@ -148,9 +173,12 @@ web_server.put("/users", async(req, res) => {
         //make sure the role is a number and exists
         if (isNaN(role)) return res.status(400).send("Invalid role");
         if (role < 0 || role > 3) return res.status(400).send("Invalid role");
-    
-        const user = await db_users.findOne({ username });
-        if (user) return res.status(409).send("Username already taken");
+
+        const username_valid = await validateUsername(username);
+        if (!username_valid.valid) return res.status(400).send(username_valid.error);
+
+        const password_valid = validatePassword(password);
+        if (!password_valid.valid) return res.status(400).send(password_valid.error);
     
         // hash the password with 10 rounds of salt
         const hashed_password = await bcrypt.hash(password, 10);
@@ -163,7 +191,6 @@ web_server.put("/users", async(req, res) => {
 
         return res.status(201).json({
             sucess: true,
-    
             data: new_user,
         });
     } catch(e: any) {
